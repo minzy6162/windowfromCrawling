d67d49d784321ceea13f54ef7cf12b60c341cf0e
MinJung Choi <s2019s37@e-mirim.hs.kr>
2021-04-14 16:03:32 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium.Chrome;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windowfromCrawling
+{
+    public partial class Form1 : Form
+    {
+        private ChromeDriverService _driverService = null;
+        private ChromeOptions _options = null;
+        private ChromeDriver _driver = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _driverService = ChromeDriverService.CreateDefaultService();
+            _driverService.HideCommandPromptWindow = true;
+
+            _options = new ChromeOptions();
+            _options.AddArgument("disable-gpu");
+        }
+    }
+}
